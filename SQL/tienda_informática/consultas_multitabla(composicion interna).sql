aa6e1683b1b53e54002467474f3c8fa71d99a528
Seeeeeck <fz4mbelli@gmail.com>
2026-06-15 12:32:47 -0400

    feat:fin composicion interna tienda

--- SQL/tienda_informática/consultas_multitabla(composicion interna).sql
+++ SQL/tienda_informática/consultas_multitabla(composicion interna).sql
@@ -86,3 +86,70 @@ SELECT * FROM producto AS p
          JOIN fabricante AS f
          ON p.id_fabricante=f.id
 WHERE f.nombre="Asus" OR f.nombre="Hewlett-Packard" OR f.nombre="Seagate" ;
+
+
+--Devuelve un listado con todos los productos de los
+--fabricantes Asus, Hewlett-Packardy Seagate. Utilizando el operador IN.
+
+
+SELECT * FROM producto AS p
+         JOIN fabricante AS f
+         ON p.id_fabricante=f.id
+WHERE f.nombre IN("Asus","Hewlett-Packard","Seagate") ;
+
+
+
+--Devuelve un listado con el nombre y el precio de todos
+--los productos de los fabricantes cuyo nombre termine por la vocal e.
+
+SELECT 
+p.nombre as 'producto',
+p.precio,
+f.nombre as "fabricante" 
+                         FROM producto p
+                         JOIN fabricante f
+                         ON p.id_fabricante=f.id
+WHERE f.nombre LIKE '%e';
+
+
+--Devuelve un listado con el nombre y el precio de todos 
+--los productos cuyo nombre de fabricante contenga el carácter w en su nombre.
+
+
+SELECT 
+p.nombre as "producto",
+p.precio,
+f.nombre as "fabricante"
+                      FROM producto p
+                      JOIN fabricante f
+                      ON p.id_fabricante=f.id
+WHERE f.nombre LIKE '%w%';
+
+
+--Devuelve un listado con el nombre de producto, precio y nombre de
+--fabricante, de todos los productos que tengan un precio
+--mayor o igual a 180€. Ordene el resultado en primer lugar por 
+--el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente)
+
+SELECT
+p.nombre as "producto",
+p.precio,
+f.nombre as "fabricante"
+        FROM producto p
+        JOIN fabricante f
+        ON p.id_fabricante=f.id
+WHERE p.precio>=180
+ORDER BY p.precio DESC, p.nombre ASC
+
+
+
+--Devuelve un listado con el identificador y el nombre de fabricante, 
+--solamente de aquellos fabricantes que tienen productos asociados en la base de datos.
+
+SELECT 
+DISTINCT(f.id) as "id fabricante",
+f.nombre as "fabricante" 
+                     FROM fabricante f
+                    RIGHT JOIN producto p
+                     ON p.id_fabricante= f.id
+WHERE p.id_fabricante IS NOT NULL;
